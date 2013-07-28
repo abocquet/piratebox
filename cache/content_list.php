46d80ef89f0c39441fff9ec53922a8bb13048517
alphindia <adrienbocquet38gmail.com>
2013-07-28 14:42:25 +0200

    first commit

---
+++ cache/content_list.php
@@ -0,0 +1,11 @@
+<?php
+
+	$contentList = array(
+
+		array("path" => "index.html", "name" => "Accueil"),
+		array("path" => "deposer.html", "name" => "Déposer un fichier"),
+		array("path" => "consulter.html", "name" => "Consulter les fichiers"),
+
+		array("path" => "index.php", "name" => "Administrer", "folder" => "admin/")
+
+	);
