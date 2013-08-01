eec7f089bcf1f952bcdea3d5766074a36a654f4c
alphindia <adrienbocquet38gmail.com>
2013-08-01 12:29:45 +0200

    Revert "Le cache navigateur n'était PAS une bonne idée"
    This reverts commit 4ba411a52ec1f56f32e25a7aa82b1feb19b134a3.

--- _config.php
+++ _config.php
@@ -1,7 +1,7 @@
 <?php
 $config['password'] = 'adrien' ; 
 $config['h1'] = 'CorsairesBoxes' ; 
-$config['h2'] = 'La boîte, à piratchoum !' ; 
+$config['h2'] = 'La boite, à piraates !' ; 
 $config['welcome_text'] = '<h3>Bonjour les gens</h3><br />
 Et les autres aussi ;)' ; 
 ?>
